1a77c5474817048173af63a035e284d6658d7597
Emmanuel Ofosu Koranteng <emmaofosuko@gmail.com>
2026-06-06 11:07:58 +0100

    Auto-commit: index.ts

---
+++ packages/app/src/index.ts
@@ -0,0 +1,10 @@
+// Screens
+export { LoginScreen } from './screens/LoginScreen';
+export { AdminDashboardScreen } from './screens/admin/AdminDashboardScreen';
+export { CoachDashboardScreen } from './screens/coach/CoachDashboardScreen';
+export { PlayerDashboardScreen } from './screens/player/PlayerDashboardScreen';
+export { ParentDashboardScreen } from './screens/parent/ParentDashboardScreen';
+
+// Navigation
+export { NAV_CONFIG, ROLE_DASHBOARD } from './navigation/config';
+export type { NavItem } from './navigation/config';
