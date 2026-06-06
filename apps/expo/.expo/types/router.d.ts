fc94f83dc5904bcb376d125fc1e9dc9e8daa5abd
Emmanuel Ofosu Koranteng <emmaofosuko@gmail.com>
2026-06-06 12:15:06 +0100

    Auto-commit: babel.config.js

--- apps/expo/.expo/types/router.d.ts
+++ apps/expo/.expo/types/router.d.ts
@@ -6,7 +6,7 @@ export * from 'expo-router';
 declare module 'expo-router' {
   export namespace ExpoRouter {
     export interface __routes<T extends string = string> extends Record<string, unknown> {
-      StaticRoutes: `/` | `/(auth)` | `/(auth)/login` | `/(tabs)` | `/(tabs)/` | `/(tabs)/attendance` | `/(tabs)/chat` | `/(tabs)/health` | `/(tabs)/players` | `/(tabs)/schedule` | `/(tabs)/settings` | `/_sitemap` | `/attendance` | `/chat` | `/health` | `/login` | `/players` | `/schedule` | `/settings`;
+      StaticRoutes: `/` | `/(auth)` | `/(auth)/login` | `/(tabs)` | `/(tabs)/` | `/(tabs)/attendance` | `/(tabs)/chat` | `/(tabs)/health` | `/(tabs)/players` | `/(tabs)/schedule` | `/(tabs)/settings` | `/..\..\next\.next\types\app\(auth)\layout` | `/..\..\next\.next\types\app\(auth)\login\page` | `/..\..\next\.next\types\app\layout` | `/..\..\next\.next\types\app\page` | `/_sitemap` | `/attendance` | `/chat` | `/health` | `/login` | `/players` | `/schedule` | `/settings`;
       DynamicRoutes: never;
       DynamicRouteTemplate: never;
     }
