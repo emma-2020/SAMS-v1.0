426b503c713c9eea91b55f6f079e8ffe8297ce23
Emmanuel Ofosu Koranteng <emmaofosuko@gmail.com>
2026-06-09 22:44:59 +0100

    Auto-commit: page.tsx

--- apps/next/.next/static/chunks/webpack.js
+++ apps/next/.next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "2c7dd16374cb5021"; }
+/******/ 		__webpack_require__.h = function() { return "8584f261eb2154e4"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
