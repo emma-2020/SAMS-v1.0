c43a16c6fc544214894281514f811a85e503a8de
Emmanuel Ofosu Koranteng <emmaofosuko@gmail.com>
2026-06-10 21:58:22 +0100

    Auto-commit: ParentDashboardScreen.tsx

--- apps/next/.next/static/chunks/webpack.js
+++ apps/next/.next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "a4cda98a00dfb923"; }
+/******/ 		__webpack_require__.h = function() { return "2554324c719a7ded"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
