bc9ee746004089d23bf9021e0fc7a5a001ce219f
Emmanuel Ofosu Koranteng <emmaofosuko@gmail.com>
2026-06-12 19:45:47 +0100

    Auto-commit: layout.tsx

--- apps/next/.next/static/chunks/webpack.js
+++ apps/next/.next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "46dec0089e622c4b"; }
+/******/ 		__webpack_require__.h = function() { return "1787b7f8b53bcf1b"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
