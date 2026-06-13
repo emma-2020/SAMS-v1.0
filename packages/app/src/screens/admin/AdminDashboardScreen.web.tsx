0732303cea4ee0308cc2f8a3402f39223a1a68f7
Emmanuel Ofosu Koranteng <emmaofosuko@gmail.com>
2026-06-13 12:46:03 +0100

    Auto-commit: AdminDashboardScreen.web.tsx

--- packages/app/src/screens/admin/AdminDashboardScreen.web.tsx
+++ packages/app/src/screens/admin/AdminDashboardScreen.web.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import React, { useEffect, useState } from 'react';
-import { useRouter } from 'next/navigation';
+import { useRouter } from 'solito/navigation';
 import {
   Users, Mail, CalendarDays, Shield, ArrowRight,
   CheckCircle2, Clock, UserPlus, TrendingUp,
